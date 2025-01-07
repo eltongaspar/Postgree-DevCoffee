a9b0ab215e7f2b930c8327f6e7861bcad5ba3b06
eltongaspar <eltongaspar@gmail.com>
2025-01-07 11:04:15 -0300

    ok

--- Devcoffee - Analise Fatutas Bancos e Pagamentos.sql
+++ Devcoffee - Analise Fatutas Bancos e Pagamentos.sql
@@ -63,7 +63,7 @@ select cbkl.dateacct as data_pagamento,coalesce(ci.dateinvoiced,cidate.dateinvoi
 	 		when cbkl.trxamt < 0 then 'Saida'
 	 end  as Tipo_Transacao,
 	 --cp.cof_creditdate, --anslise para devoluções de creditos 
-	 cical.invoice_list as invoice_list,cp.docstatus doc_status
+	 cical.invoice_list as invoice_list,cp.docstatus doc_status,*
 	 --cp.user1_id,cp.user2_id,ci.user1_id,ci.user2_id,cil.user1_id,cil.user2_id, --validação de centro de custos - usado para analises 
 																--	cilcc.cil_cc,calant.cacicil_cc,cdoc.user1_id,cdoc.user2_id
 from c_bankstatementline cbkl
@@ -84,9 +84,10 @@ from c_bankstatementline cbkl
 	left join c_elementvalue cc on cc.c_elementvalue_id = coalesce(cp.user1_id,cp.user2_id,ci.user1_id,ci.user2_id,cil.user1_id,cil.user2_id,
 																		cilcc.cil_cc,calant.cacicil_cc,cdoc.user1_id,cdoc.user2_id ,0) --cc valida valores de varios campos de varias tabelas
 	where cbkl.ad_client_id = 5000017 --cliente 
-	--and cbkl.c_bpartner_id  In (5142112) --parceiros 
+	and cbkl.c_bpartner_id  In (5092534) --parceiros 
 	and cbkl.isactive  = 'Y' --registro ativo
 	and cbk.docstatus in ('CO','CL') --status completo 
+	--and cp.isreceipt = 'Y'
 	--and cbkl.dateacct between current_date - interval '5 years' AND current_date
 	and cbkl.dateacct  between '2024-11-01' and '2024-11-30'
 order by cba.c_bankaccount_id,cbkl.c_bankstatementline_id ;
--- Devcoffee -- Temp.sql
+++ Devcoffee -- Temp.sql
@@ -18,13 +18,13 @@ where cc.value in ('0301','999991','999999')
 
 --Parceiros 
 select * from c_bpartner cbp
-where  cbp."name" like ('%ERMELINDA%');
+where  cbp."name" like ('%SANDRO DE SOUZA%');
 
 -- Faturas
 select ci.c_payment_id , ci.user1_id , ci.user2_id , ci.grandtotal, ci.dateinvoiced,
 * from c_invoice ci
 where ci.dateacct  between '2024-11-01' and '2024-11-30'
-	and ci.c_bpartner_id  = 5142112 or ci.c_invoice_id = 5306112;
+	and ci.c_bpartner_id  = 5092534;
 -- c_payment_id nulo
 
 
@@ -33,20 +33,20 @@ select cil.user1_id ,cil.user2_id, cil.linenetamt ,totallines,
 * from c_invoiceline cil
 	left join c_invoice ci on ci.c_invoice_id =cil.c_invoice_id 
 where ci.dateacct between '2024-11-01' and '2024-11-30'
-	and ci.c_bpartner_id = 5142112;
+	and ci.c_bpartner_id = 5092534;
 
 -- Pagamentos 
 select cp.c_invoice_id , cp.c_payment_id ,cp.user1_id , cp.user2_id , cp.datetrx,cp.payamt,cp.isreceipt, cp.cof_processing3, cp.c_charge_id ,cp.c_invoicepayschedule_id ,
 * from c_payment cp
 where cp.datetrx  between '2024-11-01' and '2024-11-30'
-	and cp.c_bpartner_id = 5142112 or cp.c_invoice_id = 5306112;
+	and cp.c_bpartner_id = 5092534;
 
 --Alocação de pagamentos linhas
  select  ca.c_allocationhdr_id,cal.c_payment_id,cal.c_invoice_id,cal.c_invoice_id,cal.c_invoicepayschedule_id,cal.amount,
  * from c_allocationline cal
  	left join c_allocationhdr ca on cal.c_allocationhdr_id = ca.c_allocationhdr_id 
 where ca.datetrx between '2024-11-01' and '2024-11-30'
-	and cal.c_bpartner_id = 5142112;
+	and cal.c_bpartner_id = 5092534;
 
 
 --Alocação de pagamentos 
@@ -58,20 +58,20 @@ select cbkl.ad_org_id ,cbkl.c_payment_id ,cbkl.c_invoice_id ,cbkl.trxamt,
 * from c_bankstatementline cbkl
 where cbkl.dateacct between '2024-11-01' and '2024-11-30'
 	and cbkl.isactive  = 'Y' --registro ativo
-	and cbkl.c_bpartner_id  = 5142112;
+	and cbkl.c_bpartner_id  = 5092534;
  
  -- Extrato 
 select * from c_bankstatement cbk
 	left join c_bankstatementline cbkl on cbkl.c_bankstatement_id = cbk.c_bankstatement_id 
 where cbk.dateacct between '2024-11-01' and '2024-11-30'
-	and cbkl.c_bpartner_id = 5146128;
+	and cbkl.c_bpartner_id = 5092534;
 
 -- agendamentos de pagamentos 
 select cips.c_payment_id, ci.c_invoice_id,
 * from c_invoicepayschedule cips
 	left join c_invoice ci on cips.c_invoice_id = ci.c_invoice_id
 where cips.duedate between '2024-11-01' and '2024-11-30'
-	and ci.c_bpartner_id = 5142112
+	and ci.c_bpartner_id = 5092534
 order by cips.c_invoice_id;
 	--and cips.c_payment_id not in (null,0);
 
@@ -80,7 +80,7 @@ order by cips.c_invoice_id;
  * from c_bankstatementline cbkl
   	left join c_allocationline cal on cal.c_payment_id  = cbkl.c_payment_id 
   	left join c_invoice ci on cal.c_invoice_id = ci.c_invoice_id 
-  where cbkl.c_bpartner_id = 5146128 and cbkl.dateacct between '2024-11-01' and '2024-12-30';
+  where cbkl.c_bpartner_id = 5092534 and cbkl.dateacct between '2024-11-01' and '2024-12-30';
  
 --Extrato linhas com inner joins para rastreamento de pagamentos antecipados 
 select cbkl.c_payment_id, cbkl.c_payment_id ,cp.c_payment_id,ci.c_invoice_id ,ci.user1_id, cp.user1_id,
@@ -89,7 +89,7 @@ from c_bankstatementline cbkl
   	left join c_allocationline cal on cal.c_payment_id  = cbkl.c_payment_id 
   	left join c_invoice ci on cal.c_invoice_id = ci.c_invoice_id 
   	left join c_payment cp on cp.c_payment_id  = cbkl.c_payment_id --cbkl.c_invoice_id 
-where cbkl.c_payment_id  in (5387246,5387362,5385530)
+where cbkl.c_bpartner_id   in (5092534)
 group by cbkl.c_payment_id, cbkl.c_payment_id ,cp.c_payment_id,
 		ci.c_invoice_id ,ci.user1_id,cp.c_payment_id;
 
@@ -106,7 +106,7 @@ SELECT al.c_payment_id,
           GROUP BY al.c_payment_id;
 
 --analise 
-select cbkl.c_payment_id,ci.c_invoice_id,ci.dateinvoiced,cips.duedate,cal.amount,ci.grandtotal,cbkl.trxamt,cp.payamt,
+select cbkl.c_payment_id,ci.c_invoice_id,ci.dateinvoiced,cips.duedate,cbkl.dateacct,cal.amount,ci.grandtotal,cbkl.trxamt,cp.payamt,
 						ROW_NUMBER() OVER (PARTITION BY cbkl.c_payment_id ORDER BY cbkl.c_payment_id) AS row_number --rank e rotulos dos dados para pegar o primeiro registro na posição
   					from c_bankstatementline cbkl
   						left join c_allocationline cal on cal.c_payment_id  = cbkl.c_payment_id --alicação de pagamentos linhas
@@ -115,9 +115,10 @@ select cbkl.c_payment_id,ci.c_invoice_id,ci.dateinvoiced,cips.duedate,cal.amount
   						left join c_invoiceline cil on cil.c_invoice_id = ci.c_invoice_id --faturas linhas
   						left join c_payment cp on cp.c_payment_id = cal.c_payment_id 
   						left join c_bankstatement cbk on cbk.c_bankstatement_id = cbkl.c_bankstatement_id --extrato bancario 
-  					where (cp.reversal_id is null) or (cp.docstatus not in ('RE')) or (cbk.docstatus not in ('RE')) or (cp.docstatus not in ('RE'))
-								or (cbkl.description not like ('%^%') or cbkl.description not like ('%<%') or cbkl.description not like ('%>%')) 
-  					group by cbkl.c_payment_id,ci.c_invoice_id,ci.dateinvoiced,cips.duedate,cal.amount,cbkl.trxamt,cp.payamt;
+  					where ((cp.reversal_id is null) or (cp.docstatus not in ('RE')) or (cbk.docstatus not in ('RE')) or (cp.docstatus not in ('RE'))
+								or (cbkl.description not like ('%^%') or cbkl.description not like ('%<%') or cbkl.description not like ('%>%')))
+						and cbkl.c_bpartner_id in (5092534)
+  					group by cbkl.c_payment_id,ci.c_invoice_id,ci.dateinvoiced,cips.duedate,cal.amount,cbkl.trxamt,cp.payamt,cbkl.dateacct;
 
 
 
